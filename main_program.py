4f4e203453da733d13c6dc97d91aea31d5eb943d
haftu-tekle <haftayteklehimanot@gmail.com>
2025-04-27 20:08:03 +0000

    Writing the logic

--- main_program.py
+++ main_program.py
@@ -1,3 +1,13 @@
-print('Hello World!')
-print('Goodbye!')
-print('Will Do this now')
+import random
+
+word=['python','coding','programming','computer','bad','ok','good']
+gusd_ltr=input('Pls enter a letter to guess')
+
+if __name__=='__main__':    
+    def hidn_word(word, gusd_ltr=''):
+        hidden=''
+        for line in word:
+            if line in gusd_ltr:
+                hidden+=line
+            else:
+                hidden+='_'
