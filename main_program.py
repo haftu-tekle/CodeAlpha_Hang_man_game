a977cd76ee10948feb05a55696a8959425ac4d0f
haftu-tekle <haftayteklehimanot@gmail.com>
2025-04-27 20:15:36 +0000

    Testing

--- main_program.py
+++ main_program.py
@@ -2,12 +2,17 @@ import random
 
 word=['python','coding','programming','computer','bad','ok','good']
 gusd_ltr=input('Pls enter a letter to guess')
+hidden=''
 
 if __name__=='__main__':    
-    def hidn_word(word, gusd_ltr=''):
-        hidden=''
+    def hidn_word():
+        gusd_ltr=input('Pls enter a letter to guess')
         for line in word:
             if line in gusd_ltr:
                 hidden+=line
             else:
                 hidden+='_'
+        return hidden
+
+
+hidn_word()
